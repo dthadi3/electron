628d97c50c9c32e5ae0f1faa5be927cd356a8ccd
Amit Sadaphule <58591516+amitsadaphule@users.noreply.github.com>
2020-08-11 11:32:23 +0530

    fix: build-script for ubuntu 16.04 and fedora 30 (#3)
    * fix: build-script for ubuntu 16.04 and fedora 30
    
    * Incorporate review comments
    
    - change build-script to run as non-root user
    - remove soft-link creation to node and python3.7

--- ppc64le_build.bash
+++ ppc64le_build.bash
@@ -1,15 +1,54 @@
 #!/bin/bash
 
 echo "#####################"
-echo "IT IS RECOMMENDED TO RUN THIS BUILD SCRIPT ON UBUNTU BIONIC!"
+echo "IT IS RECOMMENDED TO RUN THIS BUILD SCRIPT ON UBUNTU XENIAL/BIONIC OR FEDORA 30!"
 echo "#####################"
 echo "If any error occurs, please refer to https://wiki.raptorcs.com/wiki/Porting/Chromium for missing dependencies or others."
 echo "#####################"
 
 set -eux
 
-#apt-get update
-#apt-get install -y build-essential clang git vim cmake python libcups2-dev pkg-config libnss3-dev libssl-dev libglib2.0-dev libgnome-keyring-dev libpango1.0-dev libdbus-1-dev libatk1.0-dev libatk-bridge2.0-dev libgtk-3-dev libkrb5-dev libpulse-dev libxss-dev re2c subversion curl libasound2-dev libpci-dev mesa-common-dev gperf bison uuid-dev clang-format libatspi2.0-dev libnotify-dev libgconf2-dev libcap-dev libxtst-dev libxss1 python-dbusmock openjdk-8-jre ninja-build clang-format wget
+. /etc/os-release
+OS=$NAME
+OSVER=$VERSION_ID
+
+if [[ $OS == "Ubuntu" ]] && { [[ $OSVER == "16.04" ]] || [[ $OSVER == "18.04" ]]; };
+then
+    sudo -s <<EOF
+    apt-get update -y
+    apt-get install -y build-essential clang git vim cmake python bzip2 tar pkg-config libcups2-dev pkg-config libnss3-dev libssl-dev libglib2.0-dev libgnome-keyring-dev libpango1.0-dev libdbus-1-dev libatk1.0-dev libatk-bridge2.0-dev libgtk-3-dev libkrb5-dev libpulse-dev libxss-dev re2c subversion curl libasound2-dev libpci-dev mesa-common-dev gperf bison uuid-dev clang-format libatspi2.0-dev libnotify-dev libgconf2-dev libcap-dev libxtst-dev libxss1 python-dbusmock openjdk-8-jre clang-format wget libnotify-dev
+
+    apt-get install -y software-properties-common
+    add-apt-repository ppa:deadsnakes/ppa -y
+    apt-get update -y
+    apt-get install -y python3.7
+
+    if [[ $OSVER == "16.04" ]]
+    then
+        update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+        update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+    else
+        update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+        update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+    fi
+EOF
+
+    git clone https://github.com/ninja-build/ninja
+    cd ninja
+    git checkout v1.7.2
+    ./configure.py --bootstrap
+    export PATH=$(pwd):$PATH
+    cd ..
+elif [[ $OS == "Fedora" ]] && [[ $OSVER == "30" ]]
+then
+    sudo -s <<EOF
+    # Install dependencies
+    dnf install -y git wget python bzip2 tar pkgconfig atk-devel alsa-lib-devel bison binutils brlapi-devel bluez-libs-devel bzip2-devel cairo-devel cups-devel dbus-devel dbus-glib-devel expat-devel fontconfig-devel freetype-devel gcc-c++ glib2-devel glibc gperf gtk3-devel java-1.*.0-openjdk-devel libatomic libcap-devel libffi-devel libgcc libgnome-keyring-devel libjpeg-devel libstdc++ libX11-devel libXScrnSaver-devel libXtst-devel libxkbcommon-x11-devel ncurses-compat-libs nspr-devel nss-devel pam-devel pango-devel pciutils-devel pulseaudio-libs-devel zlib httpd mod_ssl php php-cli python-psutil wdiff xorg-x11-server-Xvfb clang vim cmake pkg-config krb5-devel re2c subversion curl libuuid-devel ninja-build libva-devel ccache xz patch diffutils findutils coreutils @development-tools elfutils rpm-devel rpm rpm-build fakeroot libgbm-devel libXcomposite gdk-pixbuf2-devel libnotify-devel
+EOF
+else
+    echo "Unsupported OS, please consider installing/building respective dependencies!"
+    exit 0
+fi
 
 VERSION=v10.20.1
 DISTRO=linux-ppc64le
@@ -35,7 +74,7 @@ git -C depot_tools checkout ppc64le
 
 export DEPOT_TOOLS_UPDATE=0
 
-PATH="$PATH:$(pwd)/depot_tools"
+export PATH=$(pwd)/gn/out:$PATH:$(pwd)/depot_tools
 export VPYTHON_BYPASS="manually managed python not supported by chrome operations"
 export GYP_DEFINES="disable_nacl=1"
 
@@ -67,6 +106,8 @@ LLVM_BUILD_DIR=$(pwd)
 cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="PowerPC" -DLLVM_ENABLE_PROJECTS="clang;lld" -G "Ninja" ../llvm-project/llvm
 ninja -j$(nproc)
 
+export PATH=$LLVM_BUILD_DIR/bin/:$PATH
+
 cd ../
 
 cd src
@@ -88,3 +129,4 @@ electron/script/strip-binaries.py -d out/Release
 ninja -C out/Release electron:electron_dist_zip
 
 echo "Distributable zip file located at: $(pwd)/out/Release/dist.zip"
+
